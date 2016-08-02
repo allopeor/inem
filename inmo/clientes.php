fbb68657414e65c50abc901ef39edd295a8b274a
allopeor <allopeor@hotmail.com>
2016-08-01 22:31:09 -0500

    inem copia1

---
+++ inmo/clientes.php
@@ -0,0 +1,21 @@
+<<!DOCTYPE html>
+<html>
+<head>
+	<title>clientes</title>
+</head>
+<body>
+
+<form action="">
+<input type='text' name="" placeholder='nombre' size="20">
+<input type='text' name="" placeholder='nombre' size="20">
+<input type='text' name="" placeholder='nombre' size="20">
+<input type='text' name="" placeholder='nombre' size="20">	
+<input type='text' name="" placeholder='nombre' size="20">
+<input type='text' name="" placeholder='nombre' size="20">
+<input type='text' name="" placeholder='nombre' size="20">
+
+
+</form>
+
+</body>
+</html>
